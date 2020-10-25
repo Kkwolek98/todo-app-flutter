053c0589021e8a11de87c3427b7c1ced0bf2eed5
Krzysztof Kwolek <krzysztof.kwolek@gmail.com>
2020-10-25 20:16:55 +0100

    changed title to final

--- lib/shared/title.dart
+++ lib/shared/title.dart
@@ -2,25 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AppTitle extends StatelessWidget {
-  String title;
+  final String title;
 
-  AppTitle(title) {
-    this.title = title;
-  }
+  AppTitle({this.title});
 
   @override
   Widget build(BuildContext context) {
-    return Container(alignment: Alignment.center, 
-        child: Container(
-          child: Padding(
+    return Container(
+      alignment: Alignment.center,
+      child: Container(
+        child: Padding(
             child: Text("$title",
                 style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-            padding: EdgeInsets.symmetric(vertical: 14, horizontal: 24)
-          ),
-          decoration: BoxDecoration(boxShadow: [
-            BoxShadow(color: Colors.black12, spreadRadius: 6, blurRadius: 12)
-          ], borderRadius: BorderRadius.circular(40), color: Colors.white),
-        ),
+            padding: EdgeInsets.symmetric(vertical: 14, horizontal: 24)),
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(color: Colors.black12, spreadRadius: 6, blurRadius: 12)
+        ], borderRadius: BorderRadius.circular(40), color: Colors.white),
+      ),
     );
   }
 }
