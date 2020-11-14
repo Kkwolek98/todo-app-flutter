b6aee7671cec3245cbaeab97a24416a0c5f980a6
Krzysztof Kwolek <krzysztof.kwolek98@gmail.com>
2020-11-14 17:26:47 +0100

    deleted unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:todo_list/shared/title.dart';
 import 'package:todo_list/todo_list.dart';
 
 void main() {
@@ -17,10 +16,6 @@ class TodoApp extends StatelessWidget {
         DefaultWidgetsLocalizations.delegate,
       ],
       title: 'Flutter Demo',
-      // theme: ThemeData(
-      //   primarySwatch: Colors.orange,
-      //   visualDensity: VisualDensity.adaptivePlatformDensity,
-      // ),
       home: TodoListView()
     );
   }
